d100c16035e2d82eaeebf455d6f3b941f0c8c330
Joao Araujo <joao.vieira22@fatec.sp.gov.br>
2020-11-04 15:25:55 -0300

    importação e exportação de Componente Header

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from  './src/components/Header';
+
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Text>Olá React Native!!!</Text>
+        <Header />
         <StatusBar style="auto" />
       </View>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+//Stateless | Functional
+const Header = (props) => (
+    <View>
+        <Text>Este é o nosso Header</Text>
+    </View>
+);
+
+export default Header;
