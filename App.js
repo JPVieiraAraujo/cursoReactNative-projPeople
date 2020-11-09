65e11450688b7f4e98f315e0cf0c570cc81ba42e
Joao Araujo <joao.vieira22@fatec.sp.gov.br>
2020-11-09 20:15:01 -0300

    commit da importação do React Navigation

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
+//import * as React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import PeoplePage from './src/pages/PeoplePage';
 
-export default PeoplePage;
+const AppNavigation = createStackNavigator ({
+  Main: {
+    screen: PeoplePage
+  }
+
+});
+
+const AppContainer = createAppContainer(AppNavigation);
+
+export default AppContainer;
